e4f2d7639fb6b4e423c3f2859f348691dc69b35f
Tanveer Salim <tanveerasalim@gmail.com>
2018-05-08 18:41:19 -0700

    master

--- exercise_1-19.c
+++ exercise_1-19.c
@@ -1,50 +1,4 @@
-<<<<<<< HEAD
-// exercise_1-19.c
-/* Write a function reverse(s)
- *
- * that reverses the character
- *
- * string s. Use it to write a 
- *
- * program that reverses its
- *
- * input a line at a time.
- */
 
-#include <stdio.h>
-
-void reverse(char s[]) {
-
-	int len = sizeof(s)/sizeof(char);
-
-	char reverse[len];
-	
-	int i = 0, r = len - 1;
-
-	while ( i < len ) {
-
-		reverse[i++] = s[r--];
-	}
-	i = 0;
-
-	while ( s[i++]= reverse[i] );
-}
-int main() {
-
-	char s[1000];
-
-	char *s_p = s;
-
-	while ( ( *s_p++ = getchar()) != EOF );
-
-	reverse(s);
-
-	printf("%s\n",s);
-	
-	
-	return 0;
-
-=======
 //exercise_1-19.c
 /* Write a function to
 
@@ -72,11 +26,19 @@ void reverse(char s[]) {
   
   char reverse[length];
   
-  while ( reverse[i++] = s[r--]);
-  
+  while ( i < length ) {
+
+	reverse[i++] = s[r--];
+
+  }
+
   i = 0;
   
-  while ( s[i++] = reverse[i]);
+  while ( i < length ) {
+
+	s[i++] =  reverse[i];
+
+  }
   
 }
 
@@ -85,29 +47,20 @@ int main(void) {
   
   int c = 0;
   
-   while ( (s[c] = getchar()) != EOF) {
+   while ( (s[c] = getchar()) != '\n') {
     
-    if ( s[c] == '\n') {
+    if ( s[c] == EOF) {
+        
+        return 0;
+    }
+        c++;
+        
+        }
         
         reverse(s);
 
         printf("%s\n",s);
         
-        
-        int i = 0;
-        
-        while (i < 1000) {
-            s[i++] = NULL;
-        }
-        
-        
-        c = 0;
-        continue;
-    }
-  
-  c++;
-  } 
-  
-  return 0;
->>>>>>> 24c9332cefcfc0563879a55e4ab51d5baec3cfd1
+        main();
 }
+
