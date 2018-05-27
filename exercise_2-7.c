0b20851d21f7db84053c6f4dc45f2d9b6c157e4f
Tanveer Salim <tanveerasalim@gmail.com>
2018-05-26 18:54:19 -0700

    master

--- exercise_2-7.c
+++ exercise_2-7.c
@@ -1,22 +1,62 @@
 // exercise_2-7.c
 
 #include <stdio.h>
+#include <stdbool.h>
 
-    unsigned getbits(unsigned x, int p, int n)                              {
-        
-        return (x >> (p + 1 - n)) & ~(~0<<n);
-    }
-    
-    unsigned rightrot(unsigned x, int n)                                    {
-        
-        unsigned bits_at_p = getbits(x, n-1, n);
-        
-        x = (x >> n) | (bits_at_p << 1);
-        
-        return x;
-    }
-    
-    int main() {
-        
-        printf("%o\n",rightrot(13,1));
-    }
+unsigned getbits(unsigned x, int p, int n)			{
+
+	return (x >> (p + 1 -n)) & ~(~0 << n);
+	
+	/*
+	everything before ampersand cuts off after
+	
+	the rightmost digit that must be extracted,
+	
+	whereas everything after the ampersand
+	
+	cuts off BEFORE the leftmost digit.
+	*/
+
+}
+
+unsigned invert (unsigned x, unsigned p, unsigned n)			{
+
+	unsigned bits_at_p = getbits(x,p,n);
+	
+	bool RSB = 0;
+	
+	unsigned index_of_inversion = (p-n+1);
+	
+	
+	while (bits_at_p > 0)                      {
+	    
+	    RSB = bits_at_p & 1;
+	    
+	    if (RSB == 0)    {
+	        
+	        x |= (1 << (index_of_inversion)); 
+	        
+	    }
+	    
+	    else             {
+	        
+	        x &= ~(RSB << (index_of_inversion)); 
+	    }
+	    
+	    index_of_inversion++;
+	    
+	    bits_at_p >>= 1;
+	    
+	}
+
+	return x;
+
+}
+
+int main() 							{
+
+	printf("%u\n",invert(0,31,32));
+
+   
+}
+
