705eed8ca25435486257dc680cbd4bae2aafa511
Tanveer Salim <tanveerasalim@gmail.com>
2018-05-27 22:56:16 -0700

    master

--- exercise_2-7.c
+++ exercise_2-7.c
@@ -1,62 +1,43 @@
-// exercise_2-7.c
+/******************************************************************************
 
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
 #include <stdio.h>
 #include <stdbool.h>
 
-unsigned getbits(unsigned x, int p, int n)			{
-
-	return (x >> (p + 1 -n)) & ~(~0 << n);
-	
-	/*
-	everything before ampersand cuts off after
-	
-	the rightmost digit that must be extracted,
-	
-	whereas everything after the ampersand
-	
-	cuts off BEFORE the leftmost digit.
-	*/
-
+unsigned getbits(unsigned x, int p, int n)                  {
+    
+    return (x >> (p-1+n))&~(~0<<n);
 }
 
-unsigned invert (unsigned x, unsigned p, unsigned n)			{
-
-	unsigned bits_at_p = getbits(x,p,n);
-	
-	bool RSB = 0;
-	
-	unsigned index_of_inversion = (p-n+1);
-	
-	
-	while (bits_at_p > 0)                      {
-	    
-	    RSB = bits_at_p & 1;
-	    
-	    if (RSB == 0)    {
-	        
-	        x |= (1 << (index_of_inversion)); 
-	        
-	    }
-	    
-	    else             {
-	        
-	        x &= ~(RSB << (index_of_inversion)); 
-	    }
-	    
-	    index_of_inversion++;
-	    
-	    bits_at_p >>= 1;
-	    
-	}
-
-	return x;
-
+unsigned invert(unsigned x, int p, int n)                   {
+    
+    int pos = p; bool bit_at_p = 0;
+    
+    while (pos >= (p-n+1))              {
+        
+        if ( getbits(x,pos,1) == 1)
+        {
+            x&=~(1<<pos);
+        }
+        else // getbits(x,pos,0) == 0
+        {
+            x |= (1 << pos);
+        }
+        pos--;
+    }
+        return x;
 }
 
-int main() 							{
+int main()
+{
 
-	printf("%u\n",invert(0,31,32));
+	printf("%u\n",invert(13,3,3));
 
-   
+    return 0;
 }
 
