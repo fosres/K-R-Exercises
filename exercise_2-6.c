6c6a9840fa84553f2cc52bbd6ddd8715a55aa2fa
Tanveer Salim <tanveerasalim@gmail.com>
2018-05-29 17:18:42 -0700

    master

---
+++ exercise_2-6.c
@@ -0,0 +1,27 @@
+}
+
+int main()
+{
+
+
+    printf("%u\n",~0);
+    printf("%u\n",setbits(0,0,25,07777777777));
+
+
+
+}
+exercise_2-6.c                                                                                   49,1           Bot
+ 42
+ 43
+ 44     printf("%u\n",~0);
+ 45     printf("%u\n",setbits(0,0,25,07777777777));
+ 46
+ 47
+ 48
+ 49 }
+~
+~
+~
+~
+exercise_2-6.c                                                                                   42,1           Bot
+
