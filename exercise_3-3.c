91debed46c1d1199b9ee9d2b86e11018d24aaee0
Tanveer Salim <tanveerasalim@gmail.com>
2018-06-08 18:09:25 -0700

    master

--- exercise_3-3.c
+++ exercise_3-3.c
@@ -23,10 +23,12 @@ char * expand(char * s1, char * s2)
 				}
 				else if ((s1 == s1_zero) && *s1_zero == '-')
 				{
-					*s2++ = '-';
+					//*s2++ = '-';
+					*s2 = c;
 				}
 				else if ((s1 == (s1_zero + strlen(s1_zero)-1)) && (*(s1_zero+strlen(s1_zero)-1)=='-'))
-					*s2++ = '-';
+					//*s2++ = '-';
+					*s2 = c;
 				else
 				{
 				*s2++ = c;
@@ -45,7 +47,7 @@ char * expand(char * s1, char * s2)
 
 int main()
 {
-	char input[1000] = "a-l-r";	
+	char input[1000] = "-a-l-s0-5-9-";	
 	char output[1000];	
 	printf("%s\n",expand(input,output));
 
