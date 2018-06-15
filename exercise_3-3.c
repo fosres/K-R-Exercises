9ca328d283bdd72f18c32ab7da7c9a515a947d38
Tanveer Salim <tanveerasalim@gmail.com>
2018-06-15 16:56:39 -0700

    master

--- exercise_3-3.c
+++ exercise_3-3.c
@@ -5,28 +5,43 @@ void expand(char *s1, char *s2)
 {
 	char *s1_p = s1; char *s2_p = s2; int index;
 
-	for (;*s1_p!='\0'; )
+	while (*s1_p!='\0' )
 	{
 		
     index = (int)(s1_p-s1);
 		
-		if (index==0)
+		if ((index==0)||(index==(strlen(s1)-1)))
 		{
 			*s2_p++=*s1_p++;
 		}
-    		else if (index==(strlen(s1)-1))
-    		{
-      			*s2_p=*s1_p++;
-    		}
 		else if (*s1_p!='-')
 		{
 			*s2_p++=*s1_p++;
 		}
 		else //*s1_p=='-'
 		{
+			if ((*(s1_p-1)==' ')||(*(s1_p+1)==' '))
+			{
+				*s2_p++=*s1_p;
+			}
+			else if (*(s1_p-1) <= *(s1_p+1))
+			{
+			
 			for (char c = (*(s1_p-1)+1); c < *(s1_p+1); c++ )
 			{
 				*s2_p++=c;
+			}
+			
+			}
+
+			else // (*(s1_p-1) > *(s1_p+1))
+			{
+			
+			for (char c = (*(s1_p-1)-1); c > *(s1_p+1); c-- )
+			{
+				*s2_p++=c;
+			}
+
 			}
 
 			*s1_p++;
@@ -36,21 +51,24 @@ void expand(char *s1, char *s2)
 
 int main() 
 {
-	  static char s1[1000] = "z-a-";
+	  static char s1[1000] = "-- -z-l-a- --";
+
+	  static char s2[1000];
+	  
 
 #if 0
 TEST FAILS FOR "-a-z 0-9 a-d-f [Fails here]-0-2 some text 1-1 WITH CAPITALS! 0-0 5-3 -";
 
 TEST FOR " -0-2- " would fail but if you fix above problem, this problem should also get fixed
 
-TEST REVERSE -z-a prints -zyx..a?
+TEST REVERSE "-z-a" prints -zyx..a would fail but you can fix that in the else statement
 
 #endif
-	  static char s2[1000];
 	      
 	  expand(s1,s2);
 
 	  printf("%s\n",s2);
 		  
 }
+
 
